9ccb55664051cc78162a85e67ecff286d11861c4
Bùi Minh Đức <93396463+minhduc-bui1@users.noreply.github.com>
2022-02-22 22:54:48 +0700

    Update base.js

--- base.js
+++ base.js
@@ -26,7 +26,7 @@ if (navigator.mozApps) {
         }
         else {
             var install = document.querySelector("#install"),
-                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/package.manifest";
+                manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/packaged.webapp";
             install.className = "show-install";
             install.onclick = function () {
                 var installApp = navigator.mozApps.installPackage(manifestURL);
